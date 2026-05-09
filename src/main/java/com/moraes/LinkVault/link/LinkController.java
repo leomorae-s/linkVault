3120c04af048fc93becd495becdf97231d4eb495
morae-s <dev.leonardomoraes@gmail.com>
2026-05-09 03:31:17 -0300

    added basic logs

--- src/main/java/com/moraes/LinkVault/link/LinkController.java
+++ src/main/java/com/moraes/LinkVault/link/LinkController.java
@@ -9,6 +9,8 @@ import com.moraes.LinkVault.link.dto.LinkResponseDTO;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.RequestBody;
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.net.URI;
+import java.time.Instant;
+
 import org.springframework.web.bind.annotation.PutMapping;
 
 
@@ -31,6 +35,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class LinkController {
 
     private final LinkService service;
+    private final Logger logger = Logger.getLogger(this.getClass());
 
     public LinkController(LinkService service) {
         this.service = service;
@@ -45,9 +50,11 @@ public class LinkController {
     @PostMapping("/save")
     public ResponseEntity<URI> save(@RequestBody @Valid LinkRequestDTO dto) {
         
-            LinkResponseDTO created = service.save(dto);
-            URI uri = URI.create("/link/"+created.id());
-            return ResponseEntity.created(uri).build();
+        logger.log(Level.INFO, "Requisição recebida: "+dto);
+        LinkResponseDTO created = service.save(dto);
+        URI uri = URI.create("/link/"+created.id());
+        logger.log(Level.INFO, "Requisição processada");
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping
@@ -58,11 +65,15 @@ public class LinkController {
 
     @GetMapping("/{id}")
     public ResponseEntity<LinkResponseDTO> getMethodName(@PathVariable Integer id) {
+
+        logger.log(Level.INFO, "Requisição recebida, id: "+id);
         return ResponseEntity.ok(service.getLinkById(id));
     }
     
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteLink(@PathVariable Integer id) {
+
+        logger.log(Level.INFO, "Requisição recebida, id: "+id);
         service.deleteById(id);
 
         return ResponseEntity.ok("Link excluido com sucesso");
@@ -71,7 +82,8 @@ public class LinkController {
     @PutMapping("/{id}")
     public ResponseEntity<LinkResponseDTO> update(@PathVariable Integer id, @RequestBody LinkRequestDTO link) {
         
-        
+        logger.log(Level.INFO, "Requisição recebida, id: "+id);
+
         return ResponseEntity.ok(service.update(link, id));
         
     }
--- src/main/java/com/moraes/LinkVault/link/LinkService.java
+++ src/main/java/com/moraes/LinkVault/link/LinkService.java
@@ -2,6 +2,8 @@ package com.moraes.LinkVault.link;
 
 
 
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +17,7 @@ public class LinkService {
     
 
     private final LinkRepository repository;
+    private final Logger logger = Logger.getLogger(this.getClass());
 
     public LinkService(LinkRepository repository) {
         this.repository = repository;
@@ -23,11 +26,13 @@ public class LinkService {
     public LinkResponseDTO save(LinkRequestDTO dto) {
 
         
-            LinkModel model = LinkModel.of(dto);
 
-            LinkModel created =repository.save(model);
+        logger.log(Level.INFO, "salvando objeto no banco de dados: "+dto);
+        LinkModel model = LinkModel.of(dto);
 
-            return LinkResponseDTO.from(created);
+        LinkModel created = repository.save(model);
+
+        return LinkResponseDTO.from(created);
 
     }
 
@@ -52,6 +57,8 @@ public class LinkService {
     }
 
     public LinkResponseDTO update(LinkRequestDTO dto, Integer id) {
+
+        logger.log(Level.INFO, "atualizando objeto no banco de dados: "+dto);
 
         LinkModel model = repository.findById(id).orElseThrow(() -> new EntityNotFoundException());
 
